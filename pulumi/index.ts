51fd17ac257f386bcd0c3a28c88e7d5e589e7086
Joseph Ward <213716219+ward-ware@users.noreply.github.com>
2025-06-01 23:55:14 +0000

    Add template to test S3 bucket deployment with Pulumi

--- pulumi/index.ts
+++ pulumi/index.ts
@@ -2,8 +2,38 @@ import * as pulumi from "@pulumi/pulumi";
 import * as aws from "@pulumi/aws";
 import * as awsx from "@pulumi/awsx";
 
+//Import configuration settings
+const config = new pulumi.Config();
+const path = config.get("path") || "portfolio";
+const indexDocument = config.get("indexDocument") || "index.html";
+const errorDocument = config.get("errorDocument") || "404.html";
+
 // Create an AWS resource (S3 Bucket)
-const bucket = new aws.s3.BucketV2("my-bucket");
+const bucket = new aws.s3.BucketV2("josephward-portfolio");
+
+const bucketWebsite = new aws.s3.BucketWebsiteConfigurationV2("josephward-portfolio-website", {
+    bucket: bucket.id,
+    indexDocument: {suffix: indexDocument},
+    errorDocument: {key: errorDocument},
+});
+
+// Configure ownership and permissions
+const ownershipControls = new aws.s3.BucketOwnershipControls("josephward-portfolio-ownership-controls", {
+    bucket: bucket.id,
+    rule: {
+        objectOwnership: "ObjectWriter",
+    },
+});
+
+// Configure public access
+const publicAccessBlock = new aws.s3.BucketPublicAccessBlock("josephward-portfolio-public-access-block", {
+    bucket: bucket.id,
+    blockPublicAcls: false,
+});
 
 // Export the name of the bucket
 export const bucketName = bucket.id;
+
+// Export the URLs and hostnames of the bucket and distribution.
+export const originURL = pulumi.interpolate`http://${bucketWebsite.websiteEndpoint}`;
+export const originHostname = bucketWebsite.websiteEndpoint;
